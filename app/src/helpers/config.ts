936f6a1b3f775460f08b1939143992a259406566
Bartosz Adamczyk <3093076+bartoszadamczyk@users.noreply.github.com>
2021-04-19 16:01:36 +0100

    Fix URL

--- app/src/helpers/config.ts
+++ app/src/helpers/config.ts
@@ -1,6 +1,6 @@
 export const Config = {
   NodeEnv: process.env.NODE_ENV,
-  WebSocketUrl: process.env.REACT_APP_WEBSOCKET_URL2,
+  WebSocketUrl: process.env.REACT_APP_WEBSOCKET_URL,
   SentryDsn: process.env.REACT_APP_SENTRY_DSN
 }
 
