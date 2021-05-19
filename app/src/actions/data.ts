fc64e494eb7860f918d5073cf6cd145b149fddf2
Bartosz Adamczyk <3093076+bartoszadamczyk@users.noreply.github.com>
2021-05-19 01:15:38 +0100

    Fix import

--- app/src/actions/data.ts
+++ app/src/actions/data.ts
@@ -1,5 +1,5 @@
 import { NewLiveMetricAction } from "../reducers/data"
-import { LiveReadingEvent } from "../../../serverless/src/types/event"
+import { LiveReadingEvent } from "../types/event"
 
 export const createNewLiveMetric = (liveReadingEvent: LiveReadingEvent): NewLiveMetricAction => ({
   type: "NEW_LIVE_METRIC",
